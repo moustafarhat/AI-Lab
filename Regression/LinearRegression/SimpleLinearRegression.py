5ba9a516fbf05d30ff4ece6f838280937370777f
Moustafa <farhat.forall@gmail.com>
2019-04-13 20:51:32 +0200

    .

--- Regression/LinearRegression/SimpleLinearRegression.py
+++ Regression/LinearRegression/SimpleLinearRegression.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-import matplotlib
-import pandas as pd
-import pylab as pl
+from sklearn.metrics import r2_score
 import numpy as np
-
+import pandas as pd
+import sklearn
 
 df = pd.read_csv("FuelConsumptionCo2.csv")
 # take a look at the dataset
@@ -38,21 +37,20 @@ msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
 test = cdf[~msk]
 
-from sklearn import linear_model
-regr = linear_model.LinearRegression()
+regr = sklearn.linear_model.LinearRegression()
 train_x = np.asanyarray(train[['ENGINESIZE']])
 train_y = np.asanyarray(train[['CO2EMISSIONS']])
 regr.fit (train_x, train_y)
+
 # The coefficients
-print ('Coefficients: ', regr.coef_)
-print ('Intercept: ',regr.intercept_)
+print('Coefficients: ', regr.coef_)
+print('Intercept: ',regr.intercept_)
 
 plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 
-from sklearn.metrics import r2_score
 
 test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
@@ -60,4 +58,4 @@ test_y_hat = regr.predict(test_x)
 
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
-print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )
+print("R2-score: %.2f" % r2_score(test_y_hat, test_y))
